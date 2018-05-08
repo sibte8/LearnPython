1dcee5286d3686f8bb2271e12dc1752a741a6f16
sibte8 <sibte8@gmail.com>
2018-05-08 14:48:22 +0800

    猜數字
    random, randint, if, elif, else

--- 條件嵌套(猜數字).py
+++ 條件嵌套(猜數字).py
@@ -9,8 +9,8 @@ from random import randint
 x = randint(0, 300)
 digit = int(input('Please input a number between 0~300: '))
 if digit == x :
-print('Bingo!')
+    print('Bingo!')
 elif digit > x:
-print('Too large, please try again.')
+    print('Too large, please try again.')
 else:
-print('Too small, please try again.')
+    print('Too small, please try again.')
