fbb6dee35493b286192cf81c885ca17865d49b17
sibte8 <sibte8@gmail.com>
2018-05-08 15:12:07 +0800

    猜5次
    for count in range

--- 條件嵌套(猜數字).py
+++ 條件嵌套(猜數字).py
@@ -7,10 +7,11 @@ Created on Tue May  8 14:37:55 2018
 
 from random import randint
 x = randint(0, 300)
-digit = int(input('Please input a number between 0~300: '))
-if digit == x :
-    print('Bingo!')
-elif digit > x:
-    print('Too large, please try again.')
-else:
-    print('Too small, please try again.')
+for count in range(0,5): #執行5次
+    digit = int(input('Please input a number between 0~300: '))
+    if digit == x :
+        print('Bingo!')
+    elif digit > x:
+        print('Too large, please try again.')
+    else:
+        print('Too small, please try again.')
