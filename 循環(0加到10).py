4fc78fda65a8a0ba7311ea0729c5bb31b53183ed
sibte8 <sibte8@gmail.com>
2018-05-08 14:56:35 +0800

    0加到10

---
+++ 循環(0加到10).py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 14:49:56 2018
+
+@author: DesktopPC
+"""
+
+sumA=0
+j=1
+while j<10:
+    sumA+=j
+    j+=1
+print('sumA:')
+print(sumA)
+print('j:')
+print(j)
