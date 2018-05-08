7439f19c819a6d06d5f933f1bf2ab68d96cd23fb
sibte8 <sibte8@gmail.com>
2018-05-08 22:00:20 +0800

    自行決定要不要繼續玩

--- 條件嵌套(猜數字).py
+++ 條件嵌套(猜數字).py
@@ -7,11 +7,17 @@ Created on Tue May  8 14:37:55 2018
 
 from random import randint
 x = randint(0, 300)
-for count in range(0,5): #執行5次
+go = 'y'
+while (go == 'y'):
     digit = int(input('Please input a number between 0~300: '))
     if digit == x :
         print('Bingo!')
+        break
     elif digit > x:
         print('Too large, please try again.')
     else:
         print('Too small, please try again.')
+    print('Input y if you want to continue.')
+    go = input()
+else:
+    print('Goodbye!')
