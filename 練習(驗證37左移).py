b1030af3fa67751e5a03ed915bbc721feb9cac2f
sibte8 <sibte8@gmail.com>
2018-05-10 15:32:14 +0800

    驗證37左移

---
+++ 練習(驗證37左移).py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:25:26 2018
+
+@author: DesktopPC
+"""
+
+for num in range(100, 1000):
+    if num % 37 == 0:
+        num_new_1 = num % 100 *10 + num // 100
+        num_new_2 = num % 10 * 100 + num // 10
+        if num_new_1 % 37 != 0 or num_new_1 % 37 != 0:
+            print("It's a false proposition.")
+            break
+else:
+    print("It's a true proposition.")
