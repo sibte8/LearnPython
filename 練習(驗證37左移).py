5d748b015502321154140510c633ff07a73e2204
sibte8 <sibte8@gmail.com>
2018-05-10 15:34:29 +0800

    改代碼

--- 練習(驗證37左移).py
+++ 練習(驗證37左移).py
@@ -9,7 +9,7 @@ for num in range(100, 1000):
     if num % 37 == 0:
         num_new_1 = num % 100 *10 + num // 100
         num_new_2 = num % 10 * 100 + num // 10
-        if num_new_1 % 37 != 0 or num_new_1 % 37 != 0:
+        if num_new_1 % 37 != 0 or num_new_2 % 37 != 0:
             print("It's a false proposition.")
             break
 else:
